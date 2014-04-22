f47307bc3091e5183642c7b513f7c25e29151e65
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-22 21:42:35 +0900

    add constructor for strings

--- Instrumental.hpp
+++ Instrumental.hpp
@@ -7,6 +7,27 @@ namespace instmt
 	class Instrument
 	{
 	protected:
-		vector<Pitch>
+		vector<Pitch> pitches;
+
+		Instrument(const unsigned int size)
+			: pitches(size) {}
+	};
+
+	class Strings : public Instrument
+	{
+	public:
+		Strings(const ScaleMap& scaleMap, const unsigned int numberOfStrings, const unsigned int numberOfFrets, const unsigned int pitchHeightOfNextString, const vector<Pitch>& firstPitches)
+			: Instrument(numberOfStrings * numberOfFrets)
+		{
+			auto itr = firstPitches.begin();
+			for (int i = 0; itr != firstPitches.end(); ++itr, ++i)
+			{
+				auto midiNum = itr->midiNoteNumber;
+				for (int j = 0; j < numberOfFrets; ++j)
+				{
+					this->pitches[i * numberOfFrets + j] = scaleMap.GetPitch(midiNum + j);
+				}
+			}
+		}
 	};
 }
