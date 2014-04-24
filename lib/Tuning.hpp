40275e2cc66204440f5146c2242fab29780be945
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-24 17:59:37 +0900

    add half step down tuning

--- lib/Tuning.hpp
+++ lib/Tuning.hpp
@@ -9,7 +9,7 @@ namespace instmt
 	{
 	public:
 		GuitarRegularTuning(const PitchMap& map)
-			: Strings(map, 6, 24, {
+			: Strings(map, 6, 25, {
 			map.GetPitch(PitchName::E, 2),
 			map.GetPitch(PitchName::A, 2),
 			map.GetPitch(PitchName::D, 3),
@@ -17,6 +17,25 @@ namespace instmt
 			map.GetPitch(PitchName::B, 3),
 			map.GetPitch(PitchName::E, 4) })
 		{
+
+		}
+	};
+
+	/**
+	* ギターの半音下げチューニング
+	*/
+	class GuitarHalfStepDownTuning : public Strings
+	{
+	public:
+		GuitarHalfStepDownTuning(const PitchMap& map)
+			: Strings(map, 6, 25, {
+			map.GetPitch(PitchName::DSharp, 2),
+			map.GetPitch(PitchName::GSharp, 2),
+			map.GetPitch(PitchName::CSharp, 3),
+			map.GetPitch(PitchName::FSharp, 3),
+			map.GetPitch(PitchName::C, 3),
+			map.GetPitch(PitchName::DSharp, 4) })
+			{
 
 		}
 	};
@@ -36,6 +55,40 @@ namespace instmt
 			map.GetPitch(PitchName::B, 3),
 			map.GetPitch(PitchName::E, 4) })
 		{
+
+		}
+	};
+
+	/**
+	* ベースのレギュラーチューニング
+	*/
+	class BaseRegularTuning : public Strings
+	{
+	public:
+		BaseRegularTuning(const PitchMap& map)
+			: Strings(map, 4, 25, {
+			map.GetPitch(PitchName::E, 2),
+			map.GetPitch(PitchName::A, 2),
+			map.GetPitch(PitchName::D, 3),
+			map.GetPitch(PitchName::G, 3)})
+			{
+
+		}
+	};
+
+	/**
+	* ベースの半音下げチューニング
+	*/
+	class BaseHalfStepDownTuning : public Strings
+	{
+	public:
+		BaseHalfStepDownTuning(const PitchMap& map)
+			: Strings(map, 4, 25, {
+			map.GetPitch(PitchName::DSharp, 2),
+			map.GetPitch(PitchName::GSharp, 2),
+			map.GetPitch(PitchName::CSharp, 3),
+			map.GetPitch(PitchName::FSharp, 3)})
+			{
 
 		}
 	};
