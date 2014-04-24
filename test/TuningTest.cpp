37bd513aac32a6f6ef65ec90c36b09dc983cf518
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-24 18:16:28 +0900

    add TuningTest

---
+++ test/TuningTest.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace instmatetest
+{
+	TEST_CLASS(TuningTest)
+	{
+	private:
+
+	public:
+		TEST_METHOD(TestGetPitch)
+		{
+			instmt::PitchMap map;
+			
+		}
+	};
+}
