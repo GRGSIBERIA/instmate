bd728f0c66bde5538983174a1db6ba94316d77dd
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-04-24 21:34:37 +0900

    add testing

--- test/TuningTest.cpp
+++ test/TuningTest.cpp
@@ -8,12 +8,21 @@ namespace instmatetest
 	TEST_CLASS(TuningTest)
 	{
 	private:
+		void TestGetPitch(const instmt::PitchMap map, const unsigned int str, const unsigned int fret, const int target)
+		{
+			auto tune = instmt::GuitarRegularTuning(map);
+			auto pitch = tune.GetPitch(str, fret);
+			Assert::AreEqual((int)pitch.pitchName, target);
+		}
 
 	public:
 		TEST_METHOD(TestGetPitch)
 		{
 			instmt::PitchMap map;
-			
+			TestGetPitch(map, 0, 0, instmt::E);
+			TestGetPitch(map, 5, 0, instmt::E);
+			TestGetPitch(map, 1, 0, instmt::A);
+			TestGetPitch(map, 0, 24, instmt::E);
 		}
 	};
 }
