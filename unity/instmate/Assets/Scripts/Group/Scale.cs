6f098d06b13c6290ef21b1756c1cab66aa0571d9
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-10-09 14:32:44 +0900

    add chord and scale

--- unity/instmate/Assets/Scripts/Group/Scale.cs
+++ unity/instmate/Assets/Scripts/Group/Scale.cs
@@ -1,3 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+namespace musical
+{
+    public class Scale : Group
+    {
+        public Scale(Group g) : base(g)
+        {
+        }
+
+        public Scale(List<int> list) : base(list)
+        {
+        }
+
+        public Scale(List<Element> list) : base(list)
+        {
+        }
+    }
+
+    public class Chord : Group
+    {
+        public Chord(List<int> list) : base(list)
+        {
+        }
+
+        public Chord(List<Element> list) : base(list)
+        {
+        }
+
+        public Chord(Group g) : base(g)
+        {
+        }
+    }
+
+}
