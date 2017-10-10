456374670d677b092a1319aeefea0ab592390d6c
Eiichi Takebuchi <nanashi4129@gmail.com>
2017-10-10 17:42:13 +0900

    コンストラクタを充実

--- unity/instmate/Assets/Scripts/Group/Scale.cs
+++ unity/instmate/Assets/Scripts/Group/Scale.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 namespace musical
 {
+    /// <summary>
+    /// スケールを生成する
+    /// </summary>
     public class Scale : Group
     {
         /// <summary>
-        /// スケールが影響を受けているコード
+        /// スケールが影響を受けているコード，nullの場合はコードに影響を受けていない
         /// </summary>
         public Chord Chord { get; protected set; }
 
@@ -31,6 +34,11 @@ namespace musical
             this.Chord = chord;
             if (chord == null)
                 InitializeScale(chord);
+        }
+
+        public Scale(Scale scale) : base(scale.List)
+        {
+            this.Chord = scale.Chord;
         }
 
         private enum DisableType
@@ -80,20 +88,31 @@ namespace musical
         }
     }
 
+    /// <summary>
+    /// 和音を表すクラス
+    /// </summary>
     public class Chord : Group
     {
         public string Name { get; private set; }
 
-        public Chord(List<int> list) : base(list)
+        public Chord(List<int> list, string name) : base(list)
         {
+            this.Name = name;
         }
 
-        public Chord(List<Element> list) : base(list)
+        public Chord(List<Element> list, string name) : base(list)
         {
+            this.Name = name;
         }
 
-        public Chord(Group g) : base(g)
+        public Chord(Group g, string name) : base(g)
         {
+            this.Name = name;
+        }
+
+        public Chord(Chord chord) : base(chord)
+        {
+            this.Name = chord.Name;
         }
     }
 
